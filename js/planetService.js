2fd067caed79d1e8a995bb9403dea9c183126482
sbrycebarker <sbrycesti@gmail.com>
2017-03-18 22:30:15 -0600

    https2

--- js/planetService.js
+++ js/planetService.js
@@ -3,7 +3,7 @@ angular.module('myApp').service('planetService', function($http){
 this.getTat = function() {
   return $http({
     method: 'GET',
-    url: "http://swapi.co/api/planets/?page=6"
+    url: "https://swapi.co/api/planets/?page=6"
   })
 }
 
@@ -12,14 +12,14 @@ this.getTat = function() {
 this.gethoth = function() {
   return $http({
     method: 'GET',
-    url: "http://swapi.co/api/planets/?page=1"
+    url: "https://swapi.co/api/planets/?page=1"
   });
 }
 
 this.getDagobah = function() {
   return $http({
     method: 'GET',
-    url: "http://swapi.co/api/planets/?page=1"
+    url: "https://swapi.co/api/planets/?page=1"
   });
 }
 //
