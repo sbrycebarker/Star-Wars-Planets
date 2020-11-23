0e7dc14967693be0258135712ccb9e25c8f50a81
sbrycebarker <sbrycesti@gmail.com>
2020-11-22 23:40:53 -0700

    fix api

--- js/planetService.js
+++ js/planetService.js
@@ -3,28 +3,28 @@ angular.module('myApp').service('planetService', function($http){
 this.getTat = function() {
   return $http({
     method: 'GET',
-    url: "https://swapi.co/api/planets/?page=6"
+    url: "https://swapi.dev/api/planets/?page=6"
   })
 }
 
 this.gethoth = function() {
   return $http({
     method: 'GET',
-    url: "https://swapi.co/api/planets/?page=1"
+    url: "https://swapi.dev/api/planets/?page=1"
   });
 }
 
 this.getDagobah = function() {
   return $http({
     method: 'GET',
-    url: "https://swapi.co/api/planets/?page=1"
+    url: "https://swapi.dev/api/planets/?page=1"
   });
 }
 
 this.getmustafar = function() {
   return $http({
     method: 'GET',
-    url: "https://swapi.co/api/planets/?page=2"
+    url: "https://swapi.dev/api/planets/?page=2"
   });
 }
 
